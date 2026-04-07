5dd0308c7966ded663a6fc038307aaadc832f035
Rath Pascal <rath@superstes.at>
2026-04-07 18:27:12 +0200

    feat: update log-format

--- src/oxl_ansible_executor/utils/debug.py
+++ src/oxl_ansible_executor/utils/debug.py
@@ -1,8 +1,13 @@
+from os import getpid
+from datetime import datetime
+from inspect import stack as inspect_stack
+from inspect import getfile as inspect_getfile
+
+from config import LOG_TIME_FORMAT
+
+PID = getpid()
 
 
 def log(msg: str):
-    print(f'[INFO] {msg}')
-
-
-def log_error(msg: str):
-    print(f'[ERROR] {msg}')
+    caller = inspect_getfile(inspect_stack()[1][0]).rsplit('/', 1)[1].rsplit('.', 1)[0]
+    print(f'[{datetime.now().strftime(LOG_TIME_FORMAT)}] [{PID}] [DEBUG] [{caller}] {msg}')
